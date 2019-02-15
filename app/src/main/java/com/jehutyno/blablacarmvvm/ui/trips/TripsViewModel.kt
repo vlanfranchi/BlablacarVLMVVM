4858449b0acd21d48462e89eeb2e2465f0da9b3d
Valentin Lanfranchi <valentin.lanfranchi@gmail.com>
2019-02-15 17:22:48 +0100

    Retrieve data in ViewModel

--- app/src/main/java/com/jehutyno/blablacarmvvm/ui/trips/TripsViewModel.kt
+++ app/src/main/java/com/jehutyno/blablacarmvvm/ui/trips/TripsViewModel.kt
@@ -2,11 +2,48 @@ package com.jehutyno.blablacarmvvm.ui.trips
 
 import com.jehutyno.blablacarmvvm.base.BaseViewModel
 import com.jehutyno.blablacarmvvm.network.BlablacarApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class TripsViewModel: BaseViewModel() {
 
     @Inject
     lateinit var blablacarApi: BlablacarApi
+
+    private lateinit var  subscription: Disposable
+
+    init {
+        loadTrips()
+    }
+
+    private fun loadTrips() {
+        subscription = blablacarApi.getTrips("paris", "marseille")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrieveTripsStart() }
+            .doOnTerminate { onRetrieveTripsFinish() }
+            .subscribe(
+                { onRetrieveTripsSuccess() },
+                { onRetrieveTripsError() }
+            )
+    }
+
+    private fun onRetrieveTripsStart(){
+
+    }
+
+    private fun onRetrieveTripsFinish(){
+
+    }
+
+    private fun onRetrieveTripsSuccess(){
+
+    }
+
+    private fun onRetrieveTripsError(){
+
+    }
 
 }
